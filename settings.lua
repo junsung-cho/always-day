73c204500419b0411ed00b7f2c35cdc559a6ac5b
Junsung Cho <97071544+junsung-cho@users.noreply.github.com>
2024-10-31 15:33:26 +0900

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -1,9 +1,11 @@
-data:extend({
-   {
-      type = "bool-setting",
-      name = "always-day",
-      setting_type = "runtime-global",
-      default_value = true,    
-      per_user = false
-   }
-})
+data:extend(
+    {
+        {
+            type = "bool-setting",
+            name = "always-day",
+            setting_type = "runtime-global",
+            default_value = true,
+            per_user = false
+        }
+    }
+)
